d558b9890f8bcfe8ebe614d938efb6b10c13eaf8
Ties de Kock <ties@tiesdekock.nl>
2016-08-05 20:18:30 +0200

    Improve the test cases and project scaffolding.

--- examples/test_wrapper.js
+++ examples/test_wrapper.js
@@ -6,13 +6,41 @@ import attributeWrapper from '../src';
 const mapping = {
   two: 'one',
   path: {
-    second: ['path', 'first']
+    second: ['path', 'first'],
   },
+  traversing: ['childList', 'firstChild'],
 };
 
-const source = fromJS({one: 1, path: {first: 'foo'}});
+const source = {
+  one: 1,
+  path: {
+    first: 'foo',
+  },
+  childList: [
+    { firstChild: 1 },
+    { firstChild: 2 },
+    { firstChild: 3 },
+  ],
+};
 
-const proxied = new Proxy(source, attributeWrapper(mapping));
+const proxied = new Proxy(fromJS(source), attributeWrapper(mapping));
 console.log(proxied.get('two'));
 console.log(proxied.set('two', 'three').toJS());
 console.log(proxied.setIn(['path', 'second'], 'bar').toJS());
+
+console.log('===================================================================================');
+console.log('===      Proxied proxied                                                        ===');
+console.log('===================================================================================');
+//
+// It is a bit too tricky to implement propery descriptors
+//
+const proxiedObject = new Proxy(source, attributeWrapper(mapping));
+
+console.log(proxiedObject);
+
+proxiedObject.two = 3;
+
+// fails: It resolves proxiedObject.path, then assigns second on that path
+proxiedObject.path.second = 4;
+
+console.log(proxiedObject);
--- src/index.js
+++ src/index.js
@@ -1,91 +1,108 @@
-import { fromJS, Collection, Map, List } from 'immutable';
+import { fromJS, Collection, List } from 'immutable';
 
 /**
  * Handle a single attribute
  */
 function getAttributeWrapper(target, targetProp) {
-		// Unwind a list of ['path1', 'path2', 'tail'] elements
-		if (targetProp instanceof List) {
-			return targetProp.reduce((cur, value) => {
-				// If the current element is a list, take the [value] from each entry.
-				if (cur instanceof List) {
-					return cur.map(c => c[value]).toList();
-				}
-				return cur[value];
-			}, target);
-		}
-		return target[targetProp];
+  // Unwind a list of ['path1', 'path2', 'tail'] elements
+  if (targetProp instanceof List) {
+    return targetProp.reduce((cur, value) => {
+      // If the current element is a list, take the [value] from each entry.
+      if (cur instanceof List) {
+        return cur.map(c => c.get(value)).toList();
+      }
+      if (cur instanceof Array) {
+        return cur.map(c => c[value]);
+      }
+
+      if (typeof cur.get === 'function') {
+        return cur.get(value);
+      }
+      return cur[value];
+    }, target);
+  }
+  return target[targetProp];
 }
 
 /**
  * Return a proxy that only rewrites the first function argument.
  */
 function keyedIterableApplyArgListProxy(targetObj, mappings) {
-	return new Proxy(targetObj, {
-		apply(target, thisArg, argumentsList) {
-			const localArguments = argumentsList;
+  return new Proxy(targetObj, {
+    apply(target, thisArg, argumentsList) {
+      const localArguments = argumentsList;
 
-			// Only rewrite when arguments are given. However, ignore the number of
-			// arguments, since additional arguments are valid in JS. We always
-			// rewrite arg[0]
-			if (argumentsList.length > 0) {
-				const arg0 = argumentsList[0];
-				if (Array.isArray(arg0)) {
-					if (mappings.hasIn(arg0)) {
-						localArguments[0] = mappings.getIn(arg0).toJS();
-					}
-				} else {
-					const newArg = mappings.get(arg0, arg0);
-					// do not rewrite an argument to a nested dictionary
-					// { path: { child: ... } would rewrite it's 'path' token to
-					// Map { child: ...
-					if (!(newArg instanceof Collection.Keyed)) {
-						localArguments[0] = mappings.get(arg0, arg0);
-					}
-				}
-			}
+      // Only rewrite when arguments are given. However, ignore the number of
+      // arguments, since additional arguments are valid in JS. We always
+      // rewrite arg[0]
+      if (argumentsList.length > 0) {
+        const arg0 = argumentsList[0];
+        if (Array.isArray(arg0)) {
+          if (mappings.hasIn(arg0)) {
+            localArguments[0] = mappings.getIn(arg0).toJS();
+          }
+        } else {
+          const newArg = mappings.get(arg0, arg0);
+          // do not rewrite an argument to a nested dictionary
+          // { path: { child: ... } would rewrite it's 'path' token to
+          // Map { child: ...
+          if (!(newArg instanceof Collection.Keyed)) {
+            localArguments[0] = mappings.get(arg0, arg0);
+          }
+        }
+      }
 
-			return target.apply(thisArg, localArguments);
-		}
-	});
+      return target.apply(thisArg, localArguments);
+    },
+  });
 }
+
+/**
+ * Check if prop has a valid mapping
+ */
+function hasValidMapping(mappings, prop) {
+  return mappings.has(prop) && !(mappings.get(prop) instanceof Collection.Keyed);
+}
+
 
 /**
 * Rename the properties of an object
 */
 export default function attributeAdapter(initialMappings) {
-	const mappings = fromJS(initialMappings);
+  const mappings = fromJS(initialMappings);
 
   return {
-    get(target, property) {
-			if (!mappings.has(property)) {
-				// Check for Immutable Map/Record:
-				if (target instanceof Collection.Keyed) {
-					// If property is 'get'/'set'/'getIn'/'setIn', rewrite argument.
-					if (['get', 'set', 'getIn', 'setIn'].includes(property)) {
-						return keyedIterableApplyArgListProxy(target[property], mappings);
-					}
-				}
+    get(target, prop) {
+      if (!hasValidMapping(mappings, prop)) {
+        // Check for Immutable Map/Record:
+        if (target instanceof Collection.Keyed) {
+          // If property is 'get'/'set'/'getIn'/'setIn', rewrite argument.
+          if (['get', 'set', 'getIn', 'setIn'].includes(prop)) {
+            return keyedIterableApplyArgListProxy(target[prop], mappings);
+          }
+        }
 
-				return target[property];
-			}
+        return target[prop];
+      }
 
-			return getAttributeWrapper(target, mappings.get(property));
+      return getAttributeWrapper(target, mappings.get(prop));
     },
 
     /* eslint-disable no-param-reassign */
-    set(target, property, value) {
-			if (!mappings.has(property)) {
-        target[property] = value;
+    set(target, prop, value) {
+      if (!hasValidMapping(mappings, prop)) {
+        target[prop] = value;
       } else {
-        const mapping = mappings.get(property);
+        const mapping = mappings.get(prop);
 
         if (mapping instanceof List) {
-          throw new Error(`Property ${property} is a synthetic property, can set this value since it could be lost...`);
+          return false;
+          // Property ${prop} is a synthetic prop, can not be set: value would be lost
         }
-        target[mappings] = value;
+        target[mapping] = value;
       }
+      return true;
     },
     /* eslint-enable no-param-reassign */
-  };
+  }
 }
